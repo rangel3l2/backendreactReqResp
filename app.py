f0a46b65623fc62c73876ad1a3b864c2c8afe891
rangel3l2 <rangel.silva@ifms.edu.br>
2022-12-21 14:20:10 -0400

    for heroku2

--- app.py
+++ app.py
@@ -7,24 +7,14 @@ from json import *
 app = Flask(__name__)
 
 
-@app.errorhandler(404)
-def page_not_found(e):
-    # your processing here
-    return {'route': request.url, 
-            'detail':'not found, be sure its exist'}
 
 @app.route('/')
-
 def home():
     
     return '<h2>Olá mundo<h2/>'
 
-@app.route('/message', methods = ['GET', 'POST'])
-def messageHandler(listaFile = []):
-    if request.method == 'GET':
-        return '<h1>olá mundo<h1>'
-    
-        
+@app.route('/message', methods = ['POST'])
+def messageHandler(listaFile = []):   
     if request.method =='POST': 
         
             
